26691eec8ef01cc63b1d79a18f37257c6a7e2fee
mjbentley100 <54856968+mjbentley100@users.noreply.github.com>
2020-02-25 02:54:29 -0700

    added more to frontend

--- SEProject_2/src/window/FaceWindow.java
+++ SEProject_2/src/window/FaceWindow.java
@@ -16,13 +16,36 @@ public class FaceWindow extends MouseAdapter {
         this.main = main;
         testPixel = new Pixel(20, 20, 50, 50);
 
+        pixelArray = new Pixel[8][8];
 
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j <8; j++){
+                pixelArray[i][j] = new Pixel(45, 45, 50 + (50 * j), 50 + (50 * i));
+                if (j % 2 == 0) {
+                    pixelArray[i][j].setOn(false);
+                }
+            }
+        }
     }
 
 
     public void render(Graphics g) {
-        g.setColor(new Color(1,1,1));
-        g.fillRect(testPixel.getX(), testPixel.getY(), testPixel.getWidth(), testPixel.getHeight());
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+
+                if (pixelArray[i][j].getOn() == true) {
+                    g.setColor(new Color(1,1,1));
+                    g.fillRect(pixelArray[i][j].getX(), pixelArray[i][j].getY(), pixelArray[i][j].getWidth(), pixelArray[i][j].getHeight());
+
+                } else {
+                    g.setColor(new Color(255, 255, 255));
+                    g.fillRect(pixelArray[i][j].getX(), pixelArray[i][j].getY(), pixelArray[i][j].getWidth(), pixelArray[i][j].getHeight());
+                }
+
+            }
+        }
+
+
     }
 
 
