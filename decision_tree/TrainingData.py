00b2d70bbb337e4c4403ab7f34271a87d7ba1834
obrewer99 <obrewer99@gmail.com>
2020-04-21 12:37:46 -0600

    changed folder name

---
+++ decision_tree/TrainingData.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+from pydub import AudioSegment
+import mysql.connector as sql
+from python_speech_features import mfcc
+from sklearn import preprocessing
+import numpy as np
+
+#########################################################################
+# TO DO:
+#	1) Place methods inside of a single class. Make connectDB own class.
+#	2) Document code && clean up
+#########################################################################
+
+# Function for connecting to MySQL Database
+def connectDB():
+	db_connection = sql.connect(host='localhost', database='test_schema', user='test_user', password='test123')
+	return db_connection
+
+# Function that determines the classifications for audio samples
+def getClassifierArray(classifications):
+	 count = 0
+	 arr = []
+	 with open(classifications) as cl:
+		 for line in cl:
+			 line = line.rstrip()
+			 arr.append(line)
+			 count = count + 1
+	 return arr
+
+
+# Function to perform MFCC analysis on audio file to get classifying features for decision tree
+def performMFCC(audio):
+	sound = AudioSegment.from_mp3(audio)
+	data = np.frombuffer(sound.raw_data, dtype=np.int16)
+	mfccs = mfcc(data, sound.frame_rate, 0.025, 0.01, 20, nfft=1200, appendEnergy=True)
+
+	#extract classifying features form mfcc
+	mfcc_feature = preprocessing.scale(mfccs, axis=1);
+	return mfcc_feature.mean(axis=1)
+
+# Function used to import dataset into DB
+def importData(el, mfccs):
+	db_connection = connectDB()
+	cursor = db_connection.cursor()
+	#sql_stmt = "INSERT INTO `test_schema`.`test_table`(`sound`) VALUES(`%d`);"
+	#values = 1;
+	if (len(mfccs) == 12):
+		sql_stmt = "INSERT INTO test_table(`sound`, `mfcc 1`, `mfcc 2`, `mfcc 3`, `mfcc 4`, `mfcc 5`, `mfcc 6`, `mfcc 7`, `mfcc 8`, `mfcc 9`, `mfcc 10`, `mfcc 11`, `mfcc 12`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+		values = (int(el), '{:0.4f}'.format(mfccs[0] *  (10**17)), '{:0.4f}'.format(mfccs[1] *  (10**17)), '{:0.4f}'.format(mfccs[2] *  (10**17)), '{:0.4f}'.format(mfccs[3] *  (10**17)), '{:0.4f}'.format(mfccs[4] *  (10**17)), '{:0.4f}'.format(mfccs[5] *  (10**17)),
+		'{:0.4f}'.format(mfccs[6] *  (10**17)), '{:0.4f}'.format(mfccs[7] *  (10**17)), '{:0.4f}'.format(mfccs[8] *  (10**17)), '{:0.4f}'.format(mfccs[9] *  (10**17)), '{:0.4f}'.format(mfccs[10] *  (10**17)), '{:0.4f}'.format(mfccs[11] *  (10**17)))
+	if (len(mfccs) > 12):
+		sql_stmt = "INSERT INTO test_table(`sound`, `mfcc 1`, `mfcc 2`, `mfcc 3`, `mfcc 4`, `mfcc 5`, `mfcc 6`, `mfcc 7`, `mfcc 8`, `mfcc 9`, `mfcc 10`, `mfcc 11`, `mfcc 12`, `mfcc 13`, `mfcc 14`, `mfcc 15`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+		values = (int(el), '{:0.4f}'.format(mfccs[0] *  (10**17)), '{:0.4f}'.format(mfccs[1] *  (10**17)), '{:0.4f}'.format(mfccs[2] *  (10**17)), '{:0.4f}'.format(mfccs[3] *  (10**17)), '{:0.4f}'.format(mfccs[4] *  (10**17)), '{:0.4f}'.format(mfccs[5] *  (10**17)),
+		'{:0.4f}'.format(mfccs[6] *  (10**17)), '{:0.4f}'.format(mfccs[7] *  (10**17)), '{:0.4f}'.format(mfccs[8] *  (10**17)), '{:0.4f}'.format(mfccs[9] *  (10**17)), '{:0.4f}'.format(mfccs[10] *  (10**17)), '{:0.4f}'.format(mfccs[11] *  (10**17)), '{:0.4f}'.format(mfccs[12] *  (10**17)), '{:0.4f}'.format(mfccs[13] *  (10**17)), '{:0.4f}'.format(mfccs[14] *  (10**17)))
+	#if (len(mfccs) == 17):
+	cursor.execute(sql_stmt, values)
+	#cursor.execute("INSERT INTO test_schema.test_table(`sound`, `mfcc 1`) VALUES (%s, %s);" % (int(1), float(mfccs[0])))
+	db_connection.commit()
+	db_connection.close();
+
+# Main function
+def main():
+	count = 0
+	folder_no = 0
+	arr = getClassifierArray('classifications.txt')
+	for el in arr:
+		if (el != "-" and el != '0'):
+			mffcs = performMFCC("../girl_sing_sample/%s-%s/sample_%s.mp3" % (folder_no, folder_no + 49, str(count).zfill(3)))
+			#print(len(mffcs))
+			try:
+				importData(el, mffcs)
+			except:
+				continue
+			#print("../girl_sing_sample/%s-%s/sample_%s.mp3" % (folder_no, folder_no + 49, str(count).zfill(3)))
+		count = count + 1
+		if (count % 50 == 0):
+			folder_no = folder_no + 50
+
+
+
+# Calling function for Main
+if __name__=="__main__":
+	main()
---
+++ decision_tree/TreeGenerator.py
@@ -0,0 +1,117 @@
+# Import the required packages
+import numpy as np
+import pandas as pd
+import mysql.connector as sql
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from sklearn.tree import export_graphviz
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+#########################################################################
+# TO DO:
+#	1) Place methods inside of a single class. Make connectDB own class.
+#	2) Rename variable names to camelCase
+#	3) Change importdata function to accomodate new DB design
+#########################################################################
+
+# Function for connecting to MySQL Database
+def connectDB():
+	db_connection = sql.connect(host='localhost', database='test_schema', user='test_user', password='test123')
+	return db_connection
+
+# Function used to import dataset from DB
+def importdata():
+	db_connection = connectDB()
+	balance_data = pd.read_sql('SELECT * FROM test_table', con=db_connection);
+	db_connection.close();
+	return balance_data
+
+# Function to split the dataset into train/test data using cross-validation
+def splitdataset(balance_data):
+	# Separate the target variable from features
+	X = balance_data.values[:, 2:14] # Features
+	Y = balance_data.values[:, 0] # Target variable
+
+	# Split the dataset into train/test
+	X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 100)
+
+	# Decision tree generator test
+	#clf = tree.DecisionTreeClassifier(criterion = "gini",
+	#		random_state = 100, max_depth = 6, min_samples_leaf = 2, max_features="log2", min_impurity_decrease=0.5)
+	#clf = clf.fit(X, Y)
+	#tree.export_graphviz(clf, out_file='test.dot') # create graph of decision tree
+
+	return X, Y, X_train, X_test, y_train, y_test
+
+# Function to perform training using GINI index
+def train_using_gini(X_train, X_test, y_train):
+	# Create the classifier object
+	clf_gini = DecisionTreeClassifier(criterion = "gini",
+			random_state = 100, max_depth = 6, min_samples_leaf = 3, max_features="auto")
+
+	# Perform training
+	clf_gini.fit(X_train, y_train)
+	#tree.export_graphviz(clf_gini, out_file='test.dot') # create graph of decision tree
+	return clf_gini
+
+# Function to perform training using Entropy
+def train_using_entropy(X_train, X_test, y_train):
+	# Construct decision tree with entropy
+	clf_entropy = DecisionTreeClassifier(criterion = "entropy",
+					random_state = 100, max_depth = None, min_samples_leaf = 2, max_features="auto")
+
+	# Perform training
+	clf_entropy.fit(X_train, y_train)
+	return clf_entropy
+
+
+# Function to make predictions
+def prediction(X_test, clf_object):
+
+	# Predicton on test with giniIndex
+	y_pred = clf_object.predict(X_test)
+	print("Predicted values:")
+	print(y_pred)
+	return y_pred
+
+# Function to calculate accuracy
+def cal_accuracy(y_test, y_pred):
+
+	print("Confusion Matrix: ",
+		confusion_matrix(y_test, y_pred))
+
+	print ("Accuracy : ",
+	accuracy_score(y_test,y_pred)*100)
+
+	print("Report : ",
+	classification_report(y_test, y_pred, zero_division=0))
+
+# Main function
+def main():
+	# Build dataset with train/test split
+	data = importdata()
+	print(data)
+	X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
+
+	clf_gini = train_using_gini(X_train, X_test, y_train)
+	clf_entropy = train_using_entropy(X_train, X_test, y_train)
+
+	#Perform tests using Gini Index and Entropy
+	print("Results Using Gini Index:")
+
+	# Prediction using gini
+	y_pred_gini = prediction(X_test, clf_gini)
+	cal_accuracy(y_test, y_pred_gini)
+
+	print("Results Using Entropy:")
+	# Prediction using entropy
+	y_pred_entropy = prediction(X_test, clf_entropy)
+	cal_accuracy(y_test, y_pred_entropy)
+
+
+# Calling function for Main
+if __name__=="__main__":
+	main()
