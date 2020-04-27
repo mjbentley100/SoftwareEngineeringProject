e910ff53b0e3c968f2c0ec087bde110b4a3a8fe0
Fcgfyv <61255994+Fcgfyv@users.noreply.github.com>
2020-04-27 00:17:50 -0600

    Update AudioAnalyzer.py

--- decision_tree/AudioAnalyzer.py
+++ decision_tree/AudioAnalyzer.py
@@ -19,15 +19,14 @@ while(1):
         init_moddate = moddate
         # extract signal and frame rate from audio sample
         try:
+            file = open("../SEProject_2/check.txt","w")
+            file.write("Super Jam = Super Live")
+            file.close()
             sound = AudioSegment.from_wav("../SEProject_2/current.wav")
             sig = np.frombuffer(sound.raw_data, dtype=np.int16)
             rate = sound.frame_rate
             #rate, sig = wav.read('./hmm.wav')
             arr = []
-            
-            file = open("../SEProject_2/check.txt","w")
-            file.write("Super Jam = Super Live")
-            file.close()
 
             # perform analysis on audio segment
             mfccs = mfcc(sig,rate,winlen=0.025,winstep=0.01,numcep=13, nfilt=26,nfft=1106,lowfreq=0,highfreq=None,preemph=0.97, ceplifter=22,appendEnergy=True)
