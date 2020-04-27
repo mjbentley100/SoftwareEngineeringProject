e7cf87e05a6870c3695a5695e943b2e1e8710b4d
mjbentley100 <54856968+mjbentley100@users.noreply.github.com>
2020-04-26 21:02:34 -0600

    idk

--- SEProject_2/src/window/PythonProcess.java
+++ SEProject_2/src/window/PythonProcess.java
@@ -11,7 +11,7 @@ public class PythonProcess {
     public void start () {
         System.out.println("RUNNING!");
         try {
-            p = Runtime.getRuntime().exec("..\\decision_tree\\venv\\Scripts\\python.exe ..\\decision_tree\\AudioAnalyzer.py");
+            p = Runtime.getRuntime().exec("python ..\\decision_tree\\AudioAnalyzer.py");
         } catch (IOException e) {
             e.printStackTrace();
         }
